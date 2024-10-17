a9cf2693bce71effa1f41ff45ff1873d81d00c50
Vlad Likov <vlad.lykov.2016@yandex.ru>
2024-10-17 23:04:31 +0300

    Update WebController_iOS.swift

--- Library/UI/iOS/WebController_iOS.swift
+++ Library/UI/iOS/WebController_iOS.swift
@@ -1,57 +1,34 @@
 import UIKit
-import WebKit
+import SafariServices
 
-final class WebControllerIOS: UIViewController, WKNavigationDelegate, WebController {
-    
-    @IBOutlet private weak var webView: WebViewWrapperIOS?
-    @IBOutlet private weak var preloader: UIActivityIndicatorView?
+final class WebControllerIOS: SFSafariViewController, SFSafariViewControllerDelegate, WebController {
     
     private var currentRequest: URLRequest?
     private var onResult: ((WebControllerResult) -> ())?
     var onDismiss: (() -> ())?
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        view.backgroundColor = .clear
-        webView?.layer.cornerRadius = 15
-        webView?.clipsToBounds = true
-        webView?.layer.borderWidth = 1 / UIScreen.main.nativeScale
-        webView?.layer.borderColor = UIColor.lightGray.cgColor
-        webView?.webView.navigationDelegate = self
-        preloader?.color = .lightGray
-        preloader?.hidesWhenStopped = true
-    }
-    
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
+        
         onDismiss?()
     }
     
-    func load(urlRequest: URLRequest, onResult: @escaping (WebControllerResult) -> ()) {
+    init?(urlRequest: URLRequest, onResult: @escaping (WebControllerResult) -> (), onDismiss: (() -> ())?) {
+        guard let url = urlRequest.url else {
+            return nil
+        }
+        super.init(url: url, configuration: .init())
+        
+        self.delegate = self
+        
         self.currentRequest = urlRequest
         self.onResult = onResult
-        
-        DispatchQueue.main.sync {
-            preloader?.startAnimating()
-            loadCurrentRequest()
-        }
+        self.onDismiss = onDismiss
     }
     
-    func reload() {
-        loadCurrentRequest()
-    }
-    
-    private func loadCurrentRequest() {
-        guard let currentRequest = currentRequest else {
-            return
-        }
-        
-        webView?.webView.load(currentRequest)
-    }
-    
-    func goBack() {
-        webView?.webView.goBack()
-    }
+    func load(urlRequest: URLRequest, onResult: @escaping (WebControllerResult) -> ()) {}
+    func reload() {}
+    func goBack() {}
     
     func dismiss() {
         DispatchQueue.main.async {
@@ -59,16 +36,10 @@ final class WebControllerIOS: UIViewController, WKNavigationDelegate, WebControl
         }
     }
     
-    @IBAction func cancellPressed(_ sender: Any) {
-        onResult?(.error(.authorizationCancelled))
+    func safariViewController(_ controller: SFSafariViewController, initialLoadDidRedirectTo URL: URL) {
+        if URL.absoluteString.contains("access_token") {
+            onResult?(.response(URL))
+        }
     }
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation?) {
-        preloader?.stopAnimating()
-        onResult?(.response(webView.url))
-    }
-    
-    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation?, withError error: Error) {
-        onResult?(.error(.webControllerError(error)))
-    }
+
 }
